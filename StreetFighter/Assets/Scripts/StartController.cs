b426fab4b9c11963a01b03291c8b1a87969e6118
rumen1425 <71330662+rumen1425@users.noreply.github.com>
2023-03-19 19:13:05 -0400

    Start menu done

--- StreetFighter/Assets/Scripts/StartController.cs
+++ StreetFighter/Assets/Scripts/StartController.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public void Play()
     {
-        
+        SceneManager.LoadScene("Game");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void HowToPlay()
     {
-        
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
