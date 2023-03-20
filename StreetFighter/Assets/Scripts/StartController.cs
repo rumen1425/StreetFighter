cef1afc659828ae3b995fa426165beea82837db7
rumen1425 <71330662+rumen1425@users.noreply.github.com>
2023-03-19 22:18:37 -0400

    .

--- StreetFighter/Assets/Scripts/StartController.cs
+++ StreetFighter/Assets/Scripts/StartController.cs
@@ -7,7 +7,11 @@ public class StartController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
         
+=======
+        SceneManager.LoadScene("BackgroundSelect");
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
