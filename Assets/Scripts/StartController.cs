dfb283ebee5f6d3989b12a5ac4823f2600c7fd72
JeffreyYang6 <80004906+JeffreyYang6@users.noreply.github.com>
2023-03-19 22:42:32 -0400

    removed dupes

---
+++ Assets/Scripts/StartController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+<<<<<<< Updated upstream
+        
+=======
+        SceneManager.LoadScene("BackgroundSelect");
+>>>>>>> Stashed changes
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
